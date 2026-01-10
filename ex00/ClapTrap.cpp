5f1a5a5fe97e8bc29cf7aa5f3f26e7c870ad18b1
ML <louism0495@gmail.com>
2026-01-10 11:42:24 +0100

    add ex00 skeleton for ClapTrap class

---
+++ ex00/ClapTrap.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlouis <mlouis@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/10 10:52:03 by mlouis            #+#    #+#             */
+/*   Updated: 2026/01/10 11:41:09 by mlouis           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap(void)
+{
+
+}
+
+ClapTrap::~ClapTrap(void)
+{
+
+}
+
+ClapTrap& ClapTrap::operator=(const ClapTrap& ct)
+{
+
+}
---
+++ ex00/ClapTrap.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlouis <mlouis@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/10 10:52:17 by mlouis            #+#    #+#             */
+/*   Updated: 2026/01/10 11:40:25 by mlouis           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+class ClapTrap
+{
+	public:
+		ClapTrap(void);
+
+		~ClapTrap(void);
+		ClapTrap& operator=(const ClapTrap& ct);
+	private:
+
+}
+
+#endif
